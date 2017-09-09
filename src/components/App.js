88114cedbaa21268d26fbe258ca0cb10ff928dab
viniciusilveira <viniciussilveira6@gmail.com>
2017-09-09 17:54:34 -0300

    Implements Inventory and load fishes from helper sampleFishes

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 
 import Header from './Header';
+import Inventory from './Inventory';
+import Fish from './Fish';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    inventory: {}
+  };
+
+  addFish = (fish) => {
+    const fishes = {...this.state.fishes};
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    this.setState({ fishes })
+  }
+
+  loadSamples = () =>{
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render(){
     return(
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
+          <ul className="list-of-fishes">
+            {
+              Object
+                .keys(this.state.fishes)
+                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+            }
+          </ul>
         </div>
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
   }
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
+import { getFunName } from '../helpers';
+
 class StorePicker extends React.Component{
+  goToStore(event){
+    event.preventDefault();
+    // first grab the text from the box
+    const storeId = this.storeInput.value;
+    // second we're going to transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+
+  }
+
   render(){
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2> Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}
+          ref={(input) => { this.storeInput = input}} />
         <button type="submit"> Visit store -></button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
