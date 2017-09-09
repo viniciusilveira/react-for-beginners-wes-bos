da73db510c0eaf872e4d4a9e8ea94f793dfaccff
viniciusilveira <viniciussilveira6@gmail.com>
2017-09-09 19:41:57 -0300

    Adding Order component in App and Storage data in firebase.

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAO1VY0U6VuHjR06cx9KYi4PHuNvYhxsQY",
+  authDomain: "catch-of-the-day-wes-bos-47fa8.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-wes-bos-47fa8.firebaseio.com",
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 import Header from './Header';
 import Inventory from './Inventory';
+import Order from './Order';
 import Fish from './Fish';
 import sampleFishes from '../sample-fishes';
+import base from '../base';
 
 class App extends React.Component {
   state = {
     fishes: {},
-    inventory: {}
+    inventory: {},
+    order: {}
   };
+
+  componentWillMount(){
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
+      {
+        context: this,
+        state: 'fishes'
+      }
+    )
+  }
+
+  componentWillUnmount(){
+    base.removeBinding(this.ref);
+  }
 
   addFish = (fish) => {
     const fishes = {...this.state.fishes};
@@ -18,10 +34,17 @@ class App extends React.Component {
     this.setState({ fishes })
   }
 
-  loadSamples = () =>{
+  loadSamples = () => {
     this.setState({
       fishes: sampleFishes
     });
+  }
+
+  addToOrder = (key) => {
+    const order = {...this.state.order};
+    order[key] = order[key] + 1 || 1;
+
+    this.setState({ order });
   }
 
   render(){
@@ -33,10 +56,12 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+                .map(key => <Fish key={key} index={key} details={this.state.fishes[key]}
+              addToOrder={this.addToOrder}/>)
             }
           </ul>
         </div>
+        <Order fishes={this.state.fishes} order={this.state.order} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
